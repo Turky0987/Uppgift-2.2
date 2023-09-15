8ba872074d00ec8d29b469a98d5cb6453ab6572f
taal04003 <taal04003@E-PW0234KH>
2023-09-15 13:29:27 +0200

    Add project files.

---
+++ uppgift 2.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ange längden Elin hoppade (i meter): ");
+        double elinLängd = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ange längden Alma hoppade (i meter): ");
+        double almaLängd = Convert.ToDouble(Console.ReadLine());
+
+        double längreHopp = elinLängd - almaLängd;
+
+        if (längreHopp > 0)
+        {
+            Console.WriteLine($"Elin hoppade {längreHopp:F2} meter längre än Alma.");
+        }
+        else if (längreHopp < 0)
+        {
+            Console.WriteLine($"Alma hoppade {-längreHopp:F2} meter längre än Elin.");
+        }
+        else
+        {
+            Console.WriteLine("Elin och Alma hoppade lika långt.");
+        }
+    }
+}
